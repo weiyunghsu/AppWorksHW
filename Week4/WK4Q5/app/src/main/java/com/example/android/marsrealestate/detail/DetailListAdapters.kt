b208195d39bd6017b7034b9e00a428f2e3dbe8f3
Wei-Yung Hsu <weiyunghsu@gmail.com>
2021-08-22 21:09:56 +0800

    still some bugs here

--- Week4/WK4Q5/app/src/main/java/com/example/android/marsrealestate/detail/DetailListAdapters.kt
+++ Week4/WK4Q5/app/src/main/java/com/example/android/marsrealestate/detail/DetailListAdapters.kt
@@ -7,28 +7,34 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsrealestate.R
 import kotlinx.android.synthetic.main.fragment_detail.view.*
+import java.nio.file.Files.list
 
 class DetailListAdapters : RecyclerView.Adapter<DetailListAdapters.DetailViewHolder>() {
     var detailList = listOf<ClipData.Item>()
-    class DetailViewHolder (itemView: View):RecyclerView.ViewHolder(itemView){
+
+    class DetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val selectedProperty = itemView.main_photo_image
         val displayPropertyPrice = itemView.price_value_text
         val displayPropertyType = itemView.property_type_text
-        fun bind(item: ClipData.Item){
+        fun bind(item: ClipData.Item) {
             selectedProperty.setImageResource(itemView.main_photo_image)
-            displayPropertyPrice.text= itemView.price_value_text.toString()
-            displayPropertyType.text= itemView.property_type_text.toString()
+            displayPropertyPrice.text = itemView.price_value_text.toString()
+            displayPropertyType.text = itemView.property_type_text.toString()
         }
     }
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):DetailViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val listViewItem = inflater.inflate(R.layout.fragment_detail, parent, false)
-            return DetailViewHolder(listViewItem)
-        }
-        override fun getItemCount() = detailList.size
-        override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
-            holder.bind(detailList[position])
-        }
-       fun updateList(list:ArrayList<Movement>){
-            detailList = list
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val listViewItem = inflater.inflate(R.layout.fragment_detail, parent, false)
+        return DetailViewHolder(listViewItem)
+    }
+
+    override fun getItemCount() = detailList.size
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        holder.bind(detailList[position])
+    }
+
+    fun updateList(list: ArrayList<ClipData>) {
+        detailList = list
+    }
 }
